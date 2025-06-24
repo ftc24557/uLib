63b16d488334bc01009cb6b1a81e7afbbbf9dd22
heitor <heitorhilles@gmail.com>
2025-06-24 15:00:27 -0300

    v1 done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
@@ -2,14 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.teamcode.core.lib.Robot;
 import org.firstinspires.ftc.teamcode.core.lib.SubSystemGroup;
-import org.firstinspires.ftc.teamcode.subsystemGroups.Outtake.Outtake;
-import org.firstinspires.ftc.teamcode.subsystemGroups.TankDrive.Drivetrain;
 
 public class MainRobot extends Robot {
+    /*
+    Instance your subsystem groups here, example:
     public static Drivetrain drive = new Drivetrain();
-    public static Outtake outtake = new Outtake();
+    */
     public static SubSystemGroup[] subSystemGroups = {
-            outtake
+            /*
+            Place your SubsystemGroups here, example:
+            drive,
+            */
     };
     public MainRobot() {
         super(subSystemGroups);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
@@ -2,13 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.sun.tools.javac.Main;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-import org.firstinspires.ftc.teamcode.core.lib.StateMachine.Action;
 
 @TeleOp
 public class Teleop1 extends LinearOpMode {
@@ -23,12 +17,6 @@ public class Teleop1 extends LinearOpMode {
         waitForStart();
 
         while (!isStopRequested()){
-            if (gamepad1.a){
-                mainRobot.outtake.toCatch();
-            }
-            if (gamepad1.b){
-                mainRobot.outtake.toScore();
-            }
             mainRobot.Periodic(telemetry);
         }
     }
